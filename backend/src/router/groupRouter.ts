c39034606a92909c87b5d9db2c9a48b1b6536f57
Duy-Vu03-02 <duyvu11122002@gmail.com>
2024-06-10 14:20:21 +0700

    Update .gitignore

--- backend/src/router/groupRouter.ts
+++
@@ -1,7 +0,0 @@
-import express, { Request, Response } from "express";
-import { createGroupConversation } from "../controllers/ConverationController";
-export default (router: express.Router) => {
-  router.post("/group/creategroup", createGroupConversation);
-  //   router.post("/group/createmessagegroup", newMessGr);
-  //   router.post("/group/getallgroup", getAllGroup);
-};
